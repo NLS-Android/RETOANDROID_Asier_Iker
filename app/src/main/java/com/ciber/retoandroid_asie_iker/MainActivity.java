afc759258d4e945552351e81d6a08610f6bf0857
p3r4lt4 <65649780+p3r4lt4@users.noreply.github.com>
2020-05-20 12:02:49 +0200

    Initial commit

---
+++ app/src/main/java/com/ciber/retoandroid_asie_iker/MainActivity.java
@@ -0,0 +1,14 @@
+package com.ciber.retoandroid_asie_iker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
