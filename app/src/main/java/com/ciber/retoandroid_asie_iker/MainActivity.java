4e842e95a4eeebf433ce2c045016a41825c45566
Asier <asier.barranco@maristak.net>
2020-05-20 12:46:15 +0200

    prueba23

--- app/src/main/java/com/ciber/retoandroid_asie_iker/MainActivity.java
+++ app/src/main/java/com/ciber/retoandroid_asie_iker/MainActivity.java
@@ -10,5 +10,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setUpToolBar();
+        //hideToolBar();
+        //showToolBar();
     }
+    private void setUpToolBar(){
+        toolbar = findViewById(R.id.toolbar);
+        //Ponemos nuestra toolbar como ActionBar
+        setSupportActionBar(toolbar);
+
+        showHomeUpIcon();
+        setUpHomeUpIconAndColor(R.drawable.ic_camera, R.color.colorWhiteApp);
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("TAG", "homeButton - setNavigationOnClickListener");
+            }
+        });
+
+        customTitleToolBar();
+    }
+
 }
