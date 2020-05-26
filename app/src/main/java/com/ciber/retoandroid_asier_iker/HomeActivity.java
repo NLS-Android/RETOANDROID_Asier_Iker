f434490eed7f3d8bde0c3d4471d39f627e9bd0ad
p3r4lt4 <65649780+p3r4lt4@users.noreply.github.com>
2020-05-26 10:37:50 +0200

    Estructura de aplicacion completados

--- app/src/main/java/com/ciber/retoandroid_asier_iker/HomeActivity.java
+++ app/src/main/java/com/ciber/retoandroid_asier_iker/HomeActivity.java
@@ -1,14 +1,57 @@
 package com.ciber.retoandroid_asier_iker;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
-        import android.os.Bundle;
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.ciber.fragments.AllowancesFragment;
+import com.ciber.fragments.ExpensesFragment;
+import com.ciber.fragments.ProfileFragment;
 
 public class HomeActivity extends AppCompatActivity {
+
+    BottomNavigationView mBottomNavigation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        Toolbar toolbar = findViewById (R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        mBottomNavigation = (BottomNavigationView) findViewById(R.id.bottomNavigation);
+        mBottomNavigation.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                if (menuItem.getItemId() == R.id.nav_expenses){
+                    showSelectedFragment(new ExpensesFragment());
+                }
+                if (menuItem.getItemId() == R.id.nav_allowances){
+                    showSelectedFragment(new AllowancesFragment());
+                }
+                if (menuItem.getItemId() == R.id.nav_profile){
+
+                    showSelectedFragment(new ProfileFragment());
+                }
+                return true;
+            }
+        });
+    }
+
+    private void showSelectedFragment (Fragment fragment) {
+        getSupportFragmentManager().beginTransaction().replace(R.id.container,fragment)
+                .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
+                .commit();
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menusuper, menu);
+        return true;
     }
 }
