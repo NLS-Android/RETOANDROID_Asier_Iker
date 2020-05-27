9eebad0fbb87b1a04f6616c9eeb709d648892aaa
p3r4lt4 <65649780+p3r4lt4@users.noreply.github.com>
2020-05-27 13:27:24 +0200

    Fragmentos correctamente

--- app/src/main/java/com/ciber/retoandroid_asier_iker/HomeActivity.java
+++ app/src/main/java/com/ciber/retoandroid_asier_iker/HomeActivity.java
@@ -9,6 +9,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.ciber.fragments.AboutFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.ciber.fragments.AllowancesFragment;
 import com.ciber.fragments.ExpensesFragment;
@@ -17,6 +19,7 @@ import com.ciber.fragments.ProfileFragment;
 public class HomeActivity extends AppCompatActivity {
 
     BottomNavigationView mBottomNavigation;
+    BottomNavigationView ToolbarBottomNavigation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,7 +40,6 @@ public class HomeActivity extends AppCompatActivity {
                     showSelectedFragment(new AllowancesFragment());
                 }
                 if (menuItem.getItemId() == R.id.nav_profile){
-
                     showSelectedFragment(new ProfileFragment());
                 }
                 return true;
@@ -54,34 +56,5 @@ public class HomeActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu){
         getMenuInflater().inflate(R.menu.menusuper, menu);
         return true;
-    }
-
-    public boolean onOptionsItemSelected(MenuItem item){
-        switch (item.getItemId()){
-            case R.id.about:
-                Intent intent = new Intent(HomeActivity.this, AboutActivity.class);
-                startActivity(intent);
-        }
-        switch (item.getItemId()){
-            case R.id.configuration:
-                Intent intent = new Intent(HomeActivity.this, ConfigurationActivity.class);
-                startActivity(intent);
-        }
-        switch (item.getItemId()){
-            case R.id.see_allowances:
-                Intent intent = new Intent(HomeActivity.this, AllowancesActivity.class);
-                startActivity(intent);
-        }
-        switch (item.getItemId()){
-            case R.id.see_expenses:
-                Intent intent = new Intent(HomeActivity.this, ExpensesActivity.class);
-                startActivity(intent);
-        }
-        switch (item.getItemId()){
-            case R.id.my_credit_cards:
-                Intent intent = new Intent(HomeActivity.this, CreditCardsActivity.class);
-                startActivity(intent);
-        }
-        return super.onOptionsItemSelected(item);
     }
 }
