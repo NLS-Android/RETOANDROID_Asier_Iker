46607e7ebf8bc079d2a8a6771dbf452d54bb9c09
Asier <asier.barranco@maristak.net>
2020-06-03 13:03:28 +0200

    Boton Llamada

--- app/src/main/java/com/ciber/fragments/AboutFragment.java
+++ app/src/main/java/com/ciber/fragments/AboutFragment.java
@@ -1,62 +1,72 @@
 package com.ciber.fragments;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
-import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.fragment.app.Fragment;
+
 import com.ciber.retoandroid_asier_iker.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link AboutFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class AboutFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+public class AboutFragment extends Fragment {
+
 
     public AboutFragment() {
         // Required empty public constructor
-    }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment AboutFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static AboutFragment newInstance(String param1, String param2) {
-        AboutFragment fragment = new AboutFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
     }
+    private Button Bcall;
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_about, container, false);
+        View v = inflater.inflate(R.layout.fragment_about, container, false);
+        Bcall = (Button) v.findViewById(R.id.boton_call);
+        Bcall.setOnClickListener ( new View.OnClickListener () {
+            @Override
+            public void onClick(View v) {
+                open();
+
+            }
+        } );
+        return v;
+
+    }
+
+    public void open (){
+        AlertDialog.Builder builder = new AlertDialog.Builder (getActivity());
+        builder.setMessage ( "Quieres llamar a soporte?" );
+        builder.setPositiveButton ( "si", new DialogInterface.OnClickListener () {
+            @Override
+            public void onClick(DialogInterface dialog, int id) {
+                Toast.makeText ( getContext (), "Ha pulsado si",
+                        Toast.LENGTH_SHORT ).show ();
+                //LLAMAR?
+                //Intent callIntent =new Intent(Intent.ACTION_CALL);
+                //callIntent.setData( Uri.parse("tel:1515"));
+                //startActivity(callIntent);
+
+            }
+
+        } );
+        builder.setNegativeButton ( "no", new DialogInterface.OnClickListener () {
+            @Override
+            public void onClick(DialogInterface dialog, int id) {
+                Toast.makeText ( getContext (), "Ha pulsado no",
+                        Toast.LENGTH_SHORT ).show ();
+            }
+        } );
+        builder.show ();
+
+
     }
 }
