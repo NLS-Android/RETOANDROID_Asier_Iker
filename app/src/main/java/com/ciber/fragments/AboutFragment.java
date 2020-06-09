05069d2b5f1f00bf2acc1bdc35643bb0164280b3
p3r4lt4 <65649780+p3r4lt4@users.noreply.github.com>
2020-06-09 09:03:21 +0200

    Boton llamada finalizada

--- app/src/main/java/com/ciber/fragments/AboutFragment.java
+++ app/src/main/java/com/ciber/fragments/AboutFragment.java
@@ -1,7 +1,12 @@
 package com.ciber.fragments;
 
+import android.Manifest;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,13 +14,11 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.Toast;
 
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
-
 import com.ciber.retoandroid_asier_iker.R;
 
-
 public class AboutFragment extends Fragment {
-
 
     public AboutFragment() {
         // Required empty public constructor
@@ -23,50 +26,39 @@ public class AboutFragment extends Fragment {
     }
     private Button Bcall;
 
-
-
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_about, container, false);
-        Bcall = (Button) v.findViewById(R.id.boton_call);
+        Bcall = (Button) v.findViewById(R.id.btn_call);
         Bcall.setOnClickListener ( new View.OnClickListener () {
             @Override
             public void onClick(View v) {
                 open();
-
             }
         } );
         return v;
-
     }
-
-    public void open (){
-        AlertDialog.Builder builder = new AlertDialog.Builder (getActivity());
-        builder.setMessage ( "Quieres llamar a soporte?" );
-        builder.setPositiveButton ( "si", new DialogInterface.OnClickListener () {
-            @Override
-            public void onClick(DialogInterface dialog, int id) {
-                Toast.makeText ( getContext (), "Ha pulsado si",
-                        Toast.LENGTH_SHORT ).show ();
-                //LLAMAR?
-                //Intent callIntent =new Intent(Intent.ACTION_CALL);
-                //callIntent.setData( Uri.parse("tel:1515"));
-                //startActivity(callIntent);
-
+    public void open(){
+        try
+        {
+            if(Build.VERSION.SDK_INT > 22)
+            {
+                if (ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.CALL_PHONE}, 101);
+                    return;
+                }
+                Intent callIntent = new Intent(Intent.ACTION_CALL);
+                callIntent.setData(Uri.parse("tel:" + "1515"));
+                startActivity(callIntent);
             }
-
-        } );
-        builder.setNegativeButton ( "no", new DialogInterface.OnClickListener () {
-            @Override
-            public void onClick(DialogInterface dialog, int id) {
-                Toast.makeText ( getContext (), "Ha pulsado no",
-                        Toast.LENGTH_SHORT ).show ();
+            else {
+                Intent callIntent = new Intent(Intent.ACTION_CALL);
+                callIntent.setData(Uri.parse("tel:" + "1515"));
+                startActivity(callIntent);
             }
-        } );
-        builder.show ();
-
-
+        }
+        catch (Exception ex) {
+            ex.printStackTrace();
+        }
     }
 }
